34c1a514ca93348d54fdb0cc961aff71a8c09592
Hugo Iván Rodríguez Vera <hi.rodriguez1998@gmail.com>
2020-01-26 12:28:01 -0300

    Constantes y NOT NULL

--- bloquesAnonimos1.sql
+++ bloquesAnonimos1.sql
@@ -2,26 +2,28 @@
 SET SERVEROUTPUT ON
 
 /*DECLARACIÓN DE VARIABLES*/
-DECLARE
 
-    NAME VARCHAR2(100);
-    LASTNAME VARCHAR2(100);
+DECLARE
+    name       VARCHAR2(100);
+    lastname   VARCHAR2(100);
+    x          CONSTANT NUMBER := 100; /*VARIABLE NO MODIFICABLE*/
+    y          NUMBER NOT NULL := 50;  /*VARIABLE NO NULLABLE*/
 
 /*================================================*/
 /*==========INICIO BLOQUE ANÓNIMO PL/SQL==========*/
-
 BEGIN  
     
     /*ASIGNACIÓN DE VARIABLES :=*/
-    NAME:='HUGO IVÁN';
-    LASTNAME:='RODRÍGUEZ VERA';
-    
-    DBMS_OUTPUT.PUT_LINE(NAME);
-    
-    DBMS_OUTPUT.PUT_LINE(LASTNAME);
-    
-    DBMS_OUTPUT.PUT_LINE(NAME || ' ' || LASTNAME);
-
+    name := 'HUGO IVÁN';
+    lastname := 'RODRÍGUEZ VERA';
+    y := 45;
+    dbms_output.put_line(name);
+    dbms_output.put_line(lastname);
+    dbms_output.put_line(name
+                         || ' '
+                         || lastname);
+    dbms_output.put_line(x);
+    dbms_output.put_line(y);
 END;
 
 /*=============================================*/
