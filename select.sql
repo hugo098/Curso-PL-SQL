d71aad53a0862df4493cf97ba865d8a43e248f74
Hugo Iván Rodríguez Vera <hi.rodriguez1998@gmail.com>
2020-01-31 20:52:44 -0300

    Rowtype

--- select.sql
+++ select.sql
@@ -1,8 +1,9 @@
 SET SERVEROUTPUT ON
 
 DECLARE
-    salario   employees.salary%TYPE;
-    nombre    employees.first_name%TYPE;
+    salario    employees.salary%TYPE;
+    nombre     employees.first_name%TYPE;
+    empleado   employees%rowtype;
 BEGIN
     SELECT  --SOLO PUEDE DEVOLVER UNA FILA
         salary,
@@ -17,4 +18,14 @@ BEGIN
 
     dbms_output.put_line(salario);
     dbms_output.put_line(nombre);
+    SELECT
+        *
+    INTO empleado
+    FROM
+        employees
+    WHERE
+        employee_id = 100;
+
+    dbms_output.put_line(empleado.salary);
+    dbms_output.put_line(empleado.first_name);
 END;
