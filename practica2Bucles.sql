69deb043d0b4f16a6b45b21fca659954c3934ac7
Hugo Iván Rodríguez Vera <hi.rodriguez1998@gmail.com>
2020-01-31 18:44:59 -0300

    Modificación para practica 3 de bucles

--- practica2Bucles.sql
+++ practica2Bucles.sql
@@ -1,13 +1,15 @@
 SET SERVEROUTPUT ON
 
 DECLARE
-    texto    VARCHAR2(100) := 'HUGO RODRIGUEZ';
+    texto    VARCHAR2(100) := 'HUGO x RODRIGUEZ';
     invert   VARCHAR2(100);
     cont     NUMBER;
 BEGIN
     cont := length(texto);
     dbms_output.put_line(cont);
     WHILE ( cont > 0 ) LOOP
+        EXIT WHEN substr(texto, cont, 1) = 'x' OR substr(texto, cont, 1) = 'X';
+
         invert := invert
                   || substr(texto, cont, 1);
         cont := cont - 1;
