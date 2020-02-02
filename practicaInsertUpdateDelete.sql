0ba883d79299c72b7ff36e812c025626552f933d
Hugo Iván Rodríguez Vera <hi.rodriguez1998@gmail.com>
2020-02-02 13:05:53 -0300

    Práctica con los comandos INSERT, UPDATE Y DELETE

---
+++ practicaInsertUpdateDelete.sql
@@ -0,0 +1,63 @@
+DECLARE
+    id_depto departments.department_id%TYPE;
+BEGIN
+    SELECT
+        MAX(department_id)
+    INTO id_depto
+    FROM
+        departments;
+
+    INSERT INTO departments (
+        department_id,
+        location_id,
+        manager_id,
+        department_name
+    ) VALUES (
+        id_depto + 1,
+        1000,
+        100,
+        'INFORMATICA'
+    );
+
+    COMMIT;
+END;
+/
+
+DECLARE
+    id_depto departments.department_id%TYPE;
+BEGIN
+    SELECT
+        MAX(department_id)
+    INTO id_depto
+    FROM
+        departments;
+
+    UPDATE departments
+    SET
+        location_id = 1700
+    WHERE
+        department_id = id_depto;
+
+END;
+/
+
+DECLARE
+    id_depto departments.department_id%TYPE;
+BEGIN
+    SELECT
+        MAX(department_id)
+    INTO id_depto
+    FROM
+        departments;
+
+    DELETE FROM departments
+    WHERE
+        department_id = id_depto;
+
+END;
+/
+
+SELECT
+    *
+FROM
+    departments
