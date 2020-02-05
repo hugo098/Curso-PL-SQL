abef4db240e8f7a597791e5e237df7d8d89a61c5
Hugo Iván Rodríguez Vera <hi.rodriguez1998@gmail.com>
2020-02-05 17:48:21 -0300

    Práctica de RAISE_APPLICATION_EXCEPTION

---
+++ practicaRaiseApplicationException.sql
@@ -0,0 +1,22 @@
+SET SERVEROUTPUT ON
+
+DECLARE
+    control_regiones EXCEPTION;
+    codigo NUMBER :=201;
+BEGIN
+    IF codigo > 200 THEN
+        RAISE control_regiones;
+    ELSE
+        INSERT INTO regions VALUES (
+            codigo,
+            'PRUEBA'
+        );
+
+    END IF;
+EXCEPTION
+    WHEN control_regiones THEN
+        RAISE_APPLICATION_ERROR(-20001, 'El codigo debe ser inferior a 200');
+    WHEN OTHERS THEN
+        dbms_output.put_line(sqlcode);
+        dbms_output.put_line(sqlerrm);
+END;
