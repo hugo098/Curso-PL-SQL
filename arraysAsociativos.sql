52853d0dc3a3585796506042598cb5d61c594daa
Hugo Iván Rodríguez Vera <hi.rodriguez1998@gmail.com>
2020-02-05 23:54:54 -0300

    Arrays asociativos

---
+++ arraysAsociativos.sql
@@ -0,0 +1,50 @@
+SET SERVEROUTPUT ON
+
+DECLARE
+    TYPE departamentos IS
+        TABLE OF departments.department_name%TYPE INDEX BY PLS_INTEGER;
+    TYPE empleados IS
+        TABLE OF employees%rowtype INDEX BY PLS_INTEGER;
+    depts    departamentos;
+    emples   empleados;
+BEGIN
+
+    --TIPO SIMPLE
+    depts(1) := 'INFORMATICA';
+    depts(2) := 'RRHH';
+    dbms_output.put_line(depts(1));
+    dbms_output.put_line(depts(2));
+    dbms_output.put_line(depts.last);
+    dbms_output.put_line(depts.first);
+    IF depts.EXISTS(3) THEN
+        dbms_output.put_line(depts(3));
+    ELSE
+        dbms_output.put_line('ESE VALOR NO EXISTE');
+    END IF;
+    
+    --TIPO COMPUESTO
+
+    SELECT
+        *
+    INTO
+        emples
+    (1)
+    FROM
+        employees
+    WHERE
+        employee_id = 100;
+
+    SELECT
+        *
+    INTO
+        emples
+    (2)
+    FROM
+        employees
+    WHERE
+        employee_id = 110;
+
+    dbms_output.put_line(emples(1).first_name);
+    dbms_output.put_line(emples(2).first_name);
+END;
+/
